111aca36156c25279d5b22af1e52b302646c99f7
ashish6667 <ay1060045@gmail.com>
2025-08-29 11:54:18 +0530

    `Removed unused files and added new routes and components`

---
+++ src/components/Home/HomeHeroText.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HomeHeroText = () => {
+  return (
+    <div className='font-[font1]'>
+        <div className='text-[9.5vw] uppercase leading-[9.5vw]'>L'etincelle</div>
+        <div className='text-[9.5vw] uppercase leading-[9.5vw]'> quigeene</div>
+        <div className='text-[9.5vw] uppercase leading-[9.5vw]'> la creativite</div>
+    </div>
+  )
+}
+
+export default HomeHeroText
