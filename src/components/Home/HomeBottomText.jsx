d535a34913827485d2e7dc517d5fe0e6a2afe2f4
ashish6667 <ay1060045@gmail.com>
2025-08-30 14:50:22 +0530

    Refactor Home component layout and enhance HomeBottomText with navigation links

--- src/components/Home/HomeBottomText.jsx
+++ src/components/Home/HomeBottomText.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const HomeBottomText = () => {
   return (
-    <div>HomeBottomText</div>
+    <div className='font-[font2] flex intems-center justify-center gap-2 '>
+      <div className='border-3 hover:border-[#D3FD50] hover:text-[#D3FD50] h-44 flex items-center px-10 border-white rounded-full uppercase'>
+        <Link className='text-[6vw] mt-6' to='/Projects'>Projects</Link>
+      </div>
+      <div className='border-3 hover:border-[#D3FD50] hover:text-[#D3FD50] h-44 flex items-center px-10 border-white rounded-full uppercase'>
+        <Link className='text-[6vw] mt-6' to='/agence'>Agence</Link>
+      </div>
+    </div>
   )
 }
 
--- src/components/Home/HomeHeroText.jsx
+++ src/components/Home/HomeHeroText.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React from "react";
+import Video from "./Video";
 
 const HomeHeroText = () => {
   return (
-    <div className='font-[font1]'>
-        <div className='text-[9.5vw] uppercase leading-[9.5vw]'>L'etincelle</div>
-        <div className='text-[9.5vw] uppercase leading-[9.5vw]'> quigeene</div>
-        <div className='text-[9.5vw] uppercase leading-[9.5vw]'> la creativite</div>
+    <div className="font-[font1] pt-5 text-center">
+      <div className="text-[9.5vw] justify-center flex items-center uppercase leading-[8vw]">
+        L'etincelle
+        </div>
+      <div className="text-[9.5vw] justify-center flex items-start uppercase leading-[8vw]">
+        qui
+        <div className="h-[8vw] w-[16vw] rounded-full -mt-3 overflow-hidden">
+          <Video />
+        </div>
+        genere
+      </div>
+      <div className="text-[9.5vw] uppercase leading-[8vw]"> la creativite</div>
     </div>
   )
 }
 
-export default HomeHeroText
+export default HomeHeroText;
